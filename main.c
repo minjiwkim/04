b94ce9268ee0cd0e7f80736b7d8faceb25322295
minjiwkim <minji5489235790@gmail.com>
2023-10-08 12:47:18 +0900

    4

--- main.c
+++ main.c
@@ -6,10 +6,18 @@ int main(int argc, char *argv[])
     
     int a;
     
-    printf("input the second : ");
+    printf("input the year :");
     scanf("%i", &a);
     
-    printf("the time is %i : %i \n", a/60, a%60);
+    if ( (a%4==0 && a%100!=0) || (a%400==0) )
+    {
+         printf("%i is leap year.\n", a);
+    }
+    
+    else
+    {
+        printf("%i is not leap year.\n", a);
+    }
   
   system("PAUSE");	
   return 0;
